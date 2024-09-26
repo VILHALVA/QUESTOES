d06fd237b12438f21daa055d21077fb3dd72d698
SAMUEL™ <107000099+VILHALVA@users.noreply.github.com>
2024-09-26 18:34:28 -0500

    Update final.py

--- CODIGO/model/final.py
+++ CODIGO/model/final.py
@@ -1,7 +1,7 @@
 from config import *
 
 def final():        
-    print("=" *25, "\n  🌎CRÉDITOS:", "\n👤CRIADOR: SAMUEL MARTINS VILHALVA\n 🐱GITHUB: @VILHALVA \n📆DATA: 20/06/2022;\n👅LINGUAGEM: PYTHON\n", "=" *25)
+    print("=" *25, "\n  🌎CRÉDITOS:", "\n👤CRIADOR: SAMUEL MARTINS VILHALVA\n 🐱GITHUB: @VILHALVA \n📆DATA: 20/06/2022\n👅LINGUAGEM: PYTHON\n", "=" *25)
     sleep(2)
     print("-" *20, "\n⛔FIM DO PROGRAMA!\n".center(10), "-" *20)    
     sleep(1)
