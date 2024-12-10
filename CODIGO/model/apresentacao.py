426ad956c99f9fd5f2907f6194e9f3b26d471399
SAMUEL™ <107000099+VILHALVA@users.noreply.github.com>
2024-12-10 16:22:05 -0500

    Update apresentacao.py

--- CODIGO/model/apresentacao.py
+++ CODIGO/model/apresentacao.py
@@ -4,5 +4,5 @@ def apresentacao():
     ascii_art = pyfiglet.figlet_format("QUESTOES")
     print(ascii_art)
     sleep(2)
-    print(f"😎SEJA BEM VINDO AO BOT DE QUESTÕES. ESSE BOT FOI FEITO PELO VILHALVA (GITHUB -> @VILHALVA)")
+    print(f"\033[95m 😎SEJA BEM VINDO AO BOT DE QUESTÕES. ESSE BOT FOI FEITO PELO VILHALVA (GITHUB -> @VILHALVA) \033[0m")
     sleep(2)
